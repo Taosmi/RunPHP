c19ff3fce1b1d085d67d9c717e4f948e18fd5fa7
Miguel Angel Garcia <magarcia@taosmi.es>
2014-01-05 12:16:21 +0100

    Upgrated: it is not a static class anymore.

--- ProWeb/Router.php
+++ ProWeb/Router.php
@@ -1,116 +1,90 @@
 <?php
 
-namespace ProWeb;
-
-
 /**
- * This class analyzes the request to know which application and which 
- * controller are involved to load and run them.
- * 
+ * Analyzes the request to know which application and which controller are
+ * involved to load and run them.
+ *
  * @author Miguel Angel Garcia
- * 
+ *
  * Copyright 2012 TAOSMI Technology
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-class Router {
+namespace ProWeb {
 
     /**
      * Loads the application configuration file involved in a request.
+     * If the application configuration is not available, returns an empty array.
      * 
-     * @param request  An associative array with a request information.
-     * @return         An associative array with an application configuration.
-     * @throws         ErrorException(0001) if the request does not belong to an application.
-     * @throws         ErrorException(0002) if the configuration file is not available.
-     * @throws         ErrorException(0003) if the log folder is not available.
+     * @param array $request  A request information.
+     * @return array          An application configuration or an empty array.
      */
-    public static function getAppConfig ($request) {
-        // The request must belong to an application, otherwise throws an error.
+    function getAppConfig ($request) {
+        // The request must belong to an application.
         if (!$request['appName']) {
-            throw new ErrorException('0001');
+            return array();
         }
-        // Defines the Application path and the base HTTP URL.
-        define('APP', WEBAPPS.DIRECTORY_SEPARATOR.$request['appName']);
-        define('BASE_URL', 'http://'.$request['appName']);
-        $configFile = APP.DIRECTORY_SEPARATOR.'config.ini';
         // Loads the application configuration file.
-        Logger::sys('Loading the configuration file for "%s"', APP);
-        if (!file_exists($configFile)) {
-            throw new ErrorException('0002', array(
-                'appName' => $request['appName']
-            ));
-        }
-        $cfg = parse_ini_file($configFile, true);
-        // Checks if the log folder exists.
-        if (!is_dir(APP.$cfg['LOGS']['path'])) {
-            throw new ErrorException('0003', array(
-                'path' => APP.$cfg['LOGS']['path']
-            ));
-        }
-        // Defines the application log configuration.
-        switch ($cfg['LOGS']['logStatus']) {
-            case '0': define('LOG_LEVEL', 0); break;
-            case '1': define('LOG_LEVEL', 1); break;
-            default: define('LOG_LEVEL', 2);
+        $cfgFile = WEBAPPS.DIRECTORY_SEPARATOR.$request['appName'].DIRECTORY_SEPARATOR.'config.ini';
+        if (!file_exists($cfgFile)) {
+            return array();
         }
         // Returns the configuration properties.
-        return $cfg;
+        return parse_ini_file($cfgFile, true);
     }
 
     /**
-     * Gets the controller file involved and returns a new instance of it. Once 
-     * the request is analyzed, the framework knows which application and 
-     * controller are involved.
+     * Gets the controller file involved and returns a new instance of it. If no
+     * controller is available, returns null.
      *
-     * @param cfg      An associative array with an application configuration. 
-     * @param request  An associative array with a request information.
-     * @return         A controller object.
-     * @throws         Error404Exception if no controller is available.
+     * @param array $cfg       An application configuration.
+     * @param array $request   A request information.
+     * @return Controller      A controller object or null.
      */
-    public static function getController ($cfg, &$request) {
+    function getController ($cfg, $request) {
         // The controller root, the controller name and the extra parameters.
         $root = $cfg['PATHS']['controllers'].DIRECTORY_SEPARATOR;
         $controller = $request['controller'];
-        $extraparam = array();
-        // Tries to get a controller.
+        $extraParam = array();
+        // If it is a folder, tries the default index controller.
+        if (is_dir(APP.$root.$controller)) {
+            $controller.= DIRECTORY_SEPARATOR.'index';
+        }
+        // Tries to find a controller.
         $loops = 2;
         while ($loops && $controller) {
-            if (is_dir(APP.$root.$controller)) {
-                $controller.= DIRECTORY_SEPARATOR.'index';
-            }
             if (file_exists(APP.$root.$controller.'.php')) {
                 $request['controller'] = $controller;
-                $request['resource'] = implode($extraparam);
+                $request['resource'] = implode($extraParam);
                 // Converts the controller name to a name-space class.
                 $controllerName = substr($root.$controller, 1);
                 $controllerName = str_replace('/', '\\', $controllerName);
                 return new $controllerName($cfg, $request);
             }
             $lastSlash = strrpos($controller, '/');
-            $extraparam[] = substr($controller, $lastSlash + 1);
+            $extraParam[] = substr($controller, $lastSlash + 1);
             $controller = substr($controller, 0, $lastSlash);
             $loops -= 1;
         }
-        // No controller found for the HTTP request.
-        throw new Error404Exception($request['appName'].$request['url'], $controllerFile);
+        return null;
     }
 
     /**
-     * Analyzes and gets the request information.
+     * Analyzes the HTTP request and retrieves the request information needed.
      * 
-     * @return  An associative array with the request information.
+     * @return array  The request information.
      */
-    public static function getRequest () {
+    function getRequest () {
         // Analyzes the request.
         $request = array(
             'from' => $_SERVER['REMOTE_ADDR'],
@@ -126,9 +100,7 @@ class Router {
         if (filter_var($request['appName'], FILTER_VALIDATE_IP)) {
             $request['appName'] = $_SERVER['COMPUTERNAME'];
         }
-        // Updates the log and returns the request.
-        Logger::sys('Request from %s to "%s/%s"', $request['from'], $request['appName'], $request['controller']);
+        // Returns the request.
         return $request;
     }
 }
-?>
