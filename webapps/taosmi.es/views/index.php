78fc62627f67ffa70da05a6ccf25ea052df0bad7
Miguel Angel Garcia <magarcia@taosmi.es>
2014-03-23 13:18:34 +0100

    The HTML for the index controller.

---
+++ webapps/taosmi.es/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TAOSMI Corporation</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+    <div style="margin:auto; padding-top:100px; width:215px;">
+        <img src="/webapps/taosmi.es/resources/imgs/taosmi.png" alt="TAOSMI technology TM Logo"/>
+    </div>
+</body>
+</html>
