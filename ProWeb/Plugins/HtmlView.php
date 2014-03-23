ceb02691b0ef32624c29fdbed4451656e780a0e9
Miguel Angel Garcia <magarcia@taosmi.es>
2014-03-23 13:25:51 +0100

    The View extension is no longer an extension. It is now a plugin. There are two plugins related to views: HtmlView and JsonView.

--- ProWeb/Plugins/HtmlView.php
+++ ProWeb/Plugins/HtmlView.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace ProWeb\Extensions;
-use ProWeb;
+namespace ProWeb\Plugins;
+use ProWeb\ErrorException;
 
 /**
- * This class implements the functionality to load a file as a View. This file 
- * may have variables that will be replaced by data at render time. The view 
+ * This class implements the functionality to load an HTML file as a View. This
+ * file may have variables that will be replaced by data at render time. The view
  * must only have visual information, the business logic must take place at the 
  * domain layer.
- * 
+ *
  * @author Miguel Angel Garcia
- * 
- * Copyright 2012 TAOSMI Technology
- * 
+ *
+ * Copyright 2014 TAOSMI Technology
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,27 +28,25 @@ use ProWeb;
 class View {
 
     /**
-     * The name of the view, the view file, the view options and the variables 
-     * that will be accessible from inside the view content.
+     * The view file and the variables that will be accessible from inside the
+     * view content.
      */
-    private $name, $file, $options, $varsPage = array();
+    private $file, $varsPage = array();
 
 
     /**
      * Checks if the view file exists.
      * 
      * @param string $name     The name of the view.
-     * @param array  $options  The view options.
-     * @throws \ProWeb\ErrorException()  If the view does not exist.
+     * @throws ErrorException()  If the view does not exist.
      */
-    public function __construct ($name, &$options) {
+    public function __construct ($name) {
         // Stores private data.
         $this->name = $name;
-        $this->options = $options;
         // Checks if the view content exists.
-        $this->file = $this->options['viewsPath'].$this->name.'.php';
+        $this->file = APP.$this->name.'.php';
         if (!file_exists($this->file)) {
-            throw new ProWeb\ErrorException('1100', __('The view does not exist.', 'View'), array(
+            throw new ErrorException('1100', __('The view does not exist.', 'View'), array(
                 'view' => $this->name,
                 'file' => $this->file
             ));
@@ -82,13 +80,13 @@ class View {
      * 
      * @param string $template  The name of the template.
      * @param array  $dynData   Data that should be accessible inside the template.
-     * @throws \ProWeb\ErrorException()  If the template does not exist.
+     * @throws ErrorException()  If the template does not exist.
      */
     public function renderTemplate ($template, $dynData = null) {
         // Checks if the template content exists.
-        $templateFile = $this->options['templatesPath'].$template.'.php';
+        $templateFile = APP.$template.'.php';
         if (!file_exists($templateFile)) {
-            throw new ProWeb\ErrorException('1101', __('The template does not exist.', 'View'), array(
+            throw new ErrorException('1101', __('The template does not exist.', 'View'), array(
                 'template' => $template,
                 'file' => $templateFile
             ));
