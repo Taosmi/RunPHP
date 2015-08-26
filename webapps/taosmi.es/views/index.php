0882950003416cab3dbb06007a2a9fa233d0c36a
Miguel Angel Garcia <magarcia@taosmi.es>
2015-08-26 18:10:40 +0200

    Old Taosmi Logo replaced.

--- webapps/taosmi.es/views/index.php
+++ webapps/taosmi.es/views/index.php
@@ -3,13 +3,13 @@
 <head>
     <title>TAOSMI technology</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link type="text/css" rel="stylesheet" href="/webapps/taosmi.es/static/css/main.css"/>
+    <link type="text/css" rel="stylesheet" href="/webapps/taosmi.es/static/main.css"/>
     <script type="text/javascript" src="/webapps/taosmi.es/static/js/jquery-2.1.0.min.js"></script>
     <script type="text/javascript" src="/webapps/taosmi.es/static/js/main.js"></script>
 </head>
 <body>
     <div class="logo">
-        <img src="/webapps/taosmi.es/static/imgs/brainTaosmiLogo.png" alt="TAOSMI Logo"/>
+        <img src="/webapps/taosmi.es/static/imgs/brainTaosmiLogoBig.png" alt="TAOSMI Logo"/>
     </div>
     <div class="saying">
         <p id="quote"></p>
