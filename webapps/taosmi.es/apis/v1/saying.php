af67e4131aec04763ce218b55b7d1d3ca0232234
Miguel Angel Garcia <magarcia@taosmi.es>
2016-05-01 15:20:40 +0200

    Comments reviewed.@ :'

--- webapps/taosmi.es/apis/v1/saying.php
+++ webapps/taosmi.es/apis/v1/saying.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace apis\v1;
-use runPHP\plugins\ApiController, runPHP\Response;
-use domain\SayingRepository;
+use runPHP\ApiController, runPHP\Response;
+use domain\Saying;
 
 /**
  * The saying API.
@@ -11,13 +11,22 @@ class saying extends ApiController {
 
     /**
      * Get a random saying from the repository.
+     *
+     * @param  array  $params  Request parameters.
+     * @return Response        A response.
      */
-    public function get () {
-        // Get a random saying.
+    public function get ($params) {
+        // Get a saying repository.
         $sayingRepo = $this->repository('domain\Saying');
-        $saying = $sayingRepo->findRandom();
-        // Return the info.
-        return new Response('data', array(
+        // Get the total number of sayings.
+        $says = $sayingRepo->select('COUNT(*) as total')->to(null)->findOne();
+        // Get a random saying.
+        $randomId = mt_rand(1, $says['total']);
+        $saying = $sayingRepo->findOne(array(
+            'id' => eq($randomId)
+        ));
+        // Return the random saying.
+        return new Response(array(
             'saying' => $saying
         ));
     }
